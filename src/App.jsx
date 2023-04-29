bed0de36546b2e3754205d4d328b52e3040cf406
Nwabuisi Obinna Divine <obinnadivine107@gmail.com>
2023-04-29 16:35:30 +0100

    first commit of the day

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,30 @@ import './styles/App.css'
 
 
 function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=57a626d41d8f3f2f48aace2678eb6439"
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+    }
+  }
+
   return (
     <section className='app'>
+      <div className="search">
+        <input 
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder='Enter Location'
+        type="text" />
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">

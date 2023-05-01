a2201af7f9feed1b31fdeb0de1d7b3eb14604772
Nwabuisi Obinna Divine <obinnadivine107@gmail.com>
2023-05-01 14:03:56 +0100

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { useState } from 'react';
 import axios from 'axios';
 import './styles/App.css'
 
-
 function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
+  const [unit, setUnit] = useState('imperial')
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=57a626d41d8f3f2f48aace2678eb6439"
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=57a626d41d8f3f2f48aace2678eb6439&units=${unit}`
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
@@ -20,18 +20,21 @@ function App() {
     }
   }
 
+  const toggleUnit = () => {
+    setUnit(unit === 'imperial' ? 'metric' : 'imperial')
+  }
+
   return (
     <section className='app'>
       <div className="search">
-        <input 
-        value={location}
-        onChange={event => setLocation(event.target.value)}
-        onKeyPress={searchLocation}
-        placeholder='Enter Location'
-        type="text" 
-        spellCheck="true"
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter Location'
+          type="text"
+          spellCheck="true"
         />
-        
       </div>
       <div className="container">
         <div className="top">
@@ -39,25 +42,30 @@ function App() {
             <p>{data.name}</p>
           </div>
           <div className="temp">
-            <h1>{data.main.temp}</h1>
+            {data.main ? <h1>{data.main.temp} {unit === 'imperial' ? '˚F' : '˚C'}</h1> : null}
           </div>
           <div className="description">
-            <p>Clouds</p>
+            <p>{data.weather ? data.weather[0].description : null}</p>
           </div>
         </div>
         <div className="bottom">
           <div className="feels">
-            <span className='bold'>55˚F</span>
+            <span className='bold'>{data.main ? `${data.main.feels_like} ${unit === 'imperial' ? '˚F' : '˚C'}` : null}</span>
             <p>Dew Point</p>
           </div>
           <div className="humidity">
-            <span className='bold'>20%</span>
+            <span className='bold'>{data.main ? `${data.main.humidity}%` : null}</span>
             <p>Humidity</p>
           </div>
           <div className="wind">
-            <span className='bold'>12MPH</span>
+            <span className='bold'>{data.wind ? `${data.wind.speed} ${unit === 'imperial' ? 'MPH' : 'KPH'}` : null}</span>
             <p>Wind Speed</p>
           </div>
+        </div>
+        <div className="unit-btn">
+          <button onClick={toggleUnit}>
+            {unit === 'imperial' ? 'Change to Celsius' : 'Change to Fahrenheit'}
+          </button>
         </div>
       </div>
     </section>

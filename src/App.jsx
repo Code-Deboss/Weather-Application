e366d983c3aa3b3fa2f1616843030a31dfe0caf9
Nwabuisi Obinna Divine <obinnadivine107@gmail.com>
2023-05-02 13:03:27 +0100

    :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./styles/App.scss";
+import "./sass/App.scss";
 
 function App() {
   const [data, setData] = useState({});
